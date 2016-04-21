4cc7dc17df5c8ec1049fab7a482cdd86bd6c104d
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-04-20 22:54:31 -0700

    Brainstorming multiple rooms and hallway

--- src/containers/Dungeon.js
+++ src/containers/Dungeon.js
@@ -8,13 +8,24 @@ export default class Dungeon extends Component {
   render() {
     return (
       <div className="dungeon">
-        <div className="room" style={{
-          top: this.props.dimensions.top,
-          left: this.props.dimensions.left,
-          width: this.props.dimensions.width,
-          height: this.props.dimensions.height
-        }}
-        />
+        { this.props.rooms.map((room, index) =>
+          <div key={index} className="room" style={{
+            top: room.y1 * 10,
+            left: room.x1 * 10,
+            width: (room.y2 - room.y1) * 10,
+            height: (room.x2 - room.x1) * 10
+          }}
+          />
+        ) }
+        { this.props.halls.map((hall, index) =>
+          <div key={index} className="hall" style={{
+            top: hall.y1 * 10,
+            left: hall.x1 * 10,
+            width: hall.x2 > hall.x1 ? (hall.x2 - hall.x1) * 10 : 10,
+            height: hall.y2 > hall.y1 ? (hall.y2 - hall.y1) * 10 : 10
+          }}
+          />
+        ) }
         <Player />
       </div>
     );
@@ -23,13 +34,15 @@ export default class Dungeon extends Component {
 
 Dungeon.propTypes = {
   dungeon: PropTypes.bool,
-  dimensions: PropTypes.object,
+  rooms: PropTypes.array,
+  halls: PropTypes.array,
   SampleAction: PropTypes.func
 };
 
 function mapStateToProps(state) {
   return {
-    dimensions: state.dungeon.dimensions
+    rooms: state.dungeon.rooms,
+    halls: state.dungeon.halls
   };
 }
 
--- src/reducers/reducer_dungeon.js
+++ src/reducers/reducer_dungeon.js
@@ -1,29 +1,49 @@
-const x1 = 2;
-const y1 = 2;
-const x2 = 30;
-const y2 = 30;
+// todo decide if I want to stick with this plan of decoupled display/board
+// or map board to div/span
 
+const rooms = [
+  { x1: 2, y1: 2, x2: 30, y2: 30 },
+  { x1: 42, y1: 2, x2: 70, y2: 30 }
+];
+const halls = [
+  { x1: 30, y1: 10, x2: 42, y2: 10 }
+];
+
+// initiate board with 0's to start
 const board = [];
 for (let i = 0; i < 70; i++) {
   const row = [];
   for (let j = 0; j < 100; j++) {
-    if (i >= x1 && i < x2 && j >= y1 && j < y2) {
-      row.push(1);
-    } else {
-      row.push(0);
-    }
+    row.push(0);
   }
   board.push(row);
+}
+// this makes sense as I'm adding to it but may be too hard to reason later on.
+// rooms - replace 0 with 1 to show it as being available
+for (let i = 0; i < 70; i++) {
+  for (let j = 0; j < 100; j++) {
+    for (let k = 0, len = rooms.length; k < len; k++) {
+      if (i >= rooms[k].x1 && i < rooms[k].x2 && j >= rooms[k].y1 && j < rooms[k].y2) {
+        board[j][i] = 1;
+      }
+    }
+  }
+}
+// halls - replace 0 with 1 to show it as being available
+for (let i = 0; i < 70; i++) {
+  for (let j = 0; j < 100; j++) {
+    for (let k = 0, len = halls.length; k < len; k++) {
+      if (i >= halls[k].x1 && i <= halls[k].x2 && j >= halls[k].y1 && j <= halls[k].y2) {
+        board[j][i] = 1;
+      }
+    }
+  }
 }
 
 const initialState = {
   board,
-  dimensions: {
-    top: x1 * 10,
-    left: y1 * 10,
-    width: (x2 - x1) * 10,
-    height: (y2 - y1) * 10
-  }
+  rooms,
+  halls
 };
 
 const dungeon = (state = initialState, action) => {
