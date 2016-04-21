1cfb455bd6743c763c099f951c8af5beba34c99b
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-04-21 10:33:29 -0700

    wip middle of adding visibility

--- src/containers/Dungeon.js
+++ src/containers/Dungeon.js
@@ -22,7 +22,8 @@ export default class Dungeon extends Component {
             top: hall.y1 * 10,
             left: hall.x1 * 10,
             width: hall.x2 > hall.x1 ? (hall.x2 - hall.x1) * 10 : 10,
-            height: hall.y2 > hall.y1 ? (hall.y2 - hall.y1) * 10 : 10
+            height: hall.y2 > hall.y1 ? (hall.y2 - hall.y1) * 10 : 10,
+            backgroundColor: hall.visible ? 'rgba(125, 125, 125, 0.7)' : 'black'
           }}
           />
         ) }
