0c50f7be7b824fb711598ed3acc628a7f432d4c1
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-04-22 13:16:04 -0700

    Implemented random room location and fixed positioning

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 todo
 modal or something on load to explain rules?
 ~use arrow keys to move around and explore the dungeon
+
+randomize player start
+
+add enemies
+add weapons
+add levels
--- src/reducers/reducer_dungeon.js
+++ src/reducers/reducer_dungeon.js
@@ -1,23 +1,30 @@
+// returns an array of 9 rooms randomly sized in a 3x3 grid
+// range must cover middle of grid square to simplify aligning hallways
 const rooms = [];
 for (let i = 0; i < 9; i++) {
   rooms[i] = {
-    x1: Math.floor(Math.random() * 12) + ((i % 3) * 34),
-    x2: (32 - Math.floor(Math.random() * 12)) + ((i % 3) * 34),
+    x1: Math.ceil(Math.random() * 12) + ((i % 3) * 34), // ceil to keep off left border
+    x2: (31 - Math.floor(Math.random() * 12)) + ((i % 3) * 34),
     y1: Math.floor(Math.random() * 9) + (Math.floor(i / 3) * 24),
     y2: (22 - Math.floor(Math.random() * 9)) + (Math.floor(i / 3) * 24),
-    visible: true
+    visible: false
   };
 }
 
+// determines order that halls connect rooms
 const hallOrder = [0, 1, 2, 5, 4, 3, 6, 7, 8];
 
 // finds the shared horizontal range between two rooms
+// and returns a random number in that range
 const findSharedY = (top1, top2, bottom1, bottom2) => {
   const top = Math.max(top1, top2);
   const bottom = Math.min(bottom1, bottom2);
   const rand = Math.floor(Math.random() * (bottom - top)) + top;
   return rand;
 };
+
+// finds the shared vertical range between two rooms
+// and returns a random number in that range
 const findSharedX = (left1, left2, right1, right2) => {
   const left = Math.max(left1, left2);
   const right = Math.min(right1, right2);
@@ -25,9 +32,9 @@ const findSharedX = (left1, left2, right1, right2) => {
   return rand;
 };
 
+// returns an array of hallways that connect rooms
 const halls = [];
 for (let i = 0; i < hallOrder.length - 1; i++) {
-	// console.log(hallOrder[i], hallOrder[i+1]);
   if (Math.abs(hallOrder[i] - hallOrder[i + 1]) === 1) { // horizontal hallway
     const sharedY = findSharedY(rooms[hallOrder[i]].y1,
       rooms[hallOrder[i + 1]].y1, rooms[hallOrder[i]].y2, rooms[hallOrder[i + 1]].y2);
@@ -36,7 +43,7 @@ for (let i = 0; i < hallOrder.length - 1; i++) {
       x2: Math.max(rooms[hallOrder[i]].x1, rooms[hallOrder[i + 1]].x1) + 1,
       y1: sharedY,
       y2: sharedY,
-      visible: true
+      visible: false
     };
   } else { // vertical
     const sharedX = findSharedX(rooms[hallOrder[i]].x1,
@@ -46,7 +53,7 @@ for (let i = 0; i < hallOrder.length - 1; i++) {
       y2: Math.max(rooms[hallOrder[i]].y1, rooms[hallOrder[i + 1]].y1) + 1,
       x1: sharedX,
       x2: sharedX,
-      visible: true
+      visible: false
     };
   }
 }
@@ -61,6 +68,7 @@ for (let i = 0; i < 100; i++) {
   board.push(row);
 }
 
+// inserts 1 in spaces covered by rooms to show as valid space for movement
 for (let i = 0; i < 100; i++) {
   for (let j = 0; j < 70; j++) {
     for (let k = 0, len = rooms.length; k < len; k++) {
@@ -70,7 +78,8 @@ for (let i = 0; i < 100; i++) {
     }
   }
 }
-// halls - replace 0 with 1 to show it as being available
+
+// inserts 1 in spaces covered by halls to show as valid space for movement
 for (let i = 0; i < 100; i++) {
   for (let j = 0; j < 70; j++) {
     for (let k = 0, len = halls.length; k < len; k++) {
